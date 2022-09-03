14660684b98d7f358a620144e58f214e0d4b2d56
William Gaber de Carvalho <111815406+willzin454@users.noreply.github.com>
2022-09-03 02:03:52 -0300

    Add files via upload

---
+++ Atividade_sala.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE william;	-- a SQL criou uma primeira base de dados.
+CREATE DATABASE william; 	-- não foi possivel criar uma mesma base dados identicas. 
+
+DROP DATABASE william;      -- vai deletar a database
+DROP DATABASE william;      -- vai apresentar erro pois ja foi eleminado
+DROP DATABASE IF EXISTS william  -- remove condicionalmente a tabela somente se ela ja existe
+CREATE DATABASE william;
